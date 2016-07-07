049b6789d55c94ab6a0c196cd3163c912d98b9cc
Valerio Mariani <valerio.mariani@desy.de>
2016-07-07 11:03:53 +0200

    Fixed typos and whitespace, added missing boilerplate

--- cfelgeom.py
+++ cfelgeom.py
@@ -12,6 +12,8 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with cfelpyutils.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """
 Utilities for CrystFEL-style geometry files.
 
